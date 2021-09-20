83df724b32e4e77d85e4dc7f3c8467e8339a0ffa
JavierAlbornoz <javix781@gmail.com>
2021-09-20 16:54:42 -0300

    condicionales

---
+++ JS/condicionales.js
@@ -0,0 +1,8 @@
+let edad = Number (prompt('que edad tienes?'));
+if (edad > 18) {
+    alert('Que disfrutes la pagina :D!!!')
+} else if(edad == 18) {
+    alert('Pasaste raspando XD')
+} else {
+    alert('raja de aqui! ¬¬')
+}
