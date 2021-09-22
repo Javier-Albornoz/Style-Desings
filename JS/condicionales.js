d0926c2eb2a71a29a77b277103c079fa35d0ef45
JavierAlbornoz <javix781@gmail.com>
2021-09-22 14:16:48 -0300

    dandole un toque de onda

--- JS/condicionales.js
+++ JS/condicionales.js
@@ -1,8 +1,20 @@
 let edad = Number (prompt('que edad tienes?'));
-if (edad > 18) {
-    alert('Que disfrutes la pagina :D!!!')
-} else if(edad == 18) {
-    alert('Pasaste raspando XD')
+if (edad < 18) {
+    alert('Tomátelas! ¬¬')
 } else {
-    alert('raja de aqui! ¬¬')
-}
+    alert('Podes proceder con la compra :D !!!')
+    const cantProductos = Number ( prompt('que cantidad de productos quieres adquirir?'));
+    const precioProductos = Number(prompt('ingrese el precio del producto'));
+    const precioFinal = (cantProductos * precioProductos);
+    alert ('el costo total es ' + precioFinal);
+    const quieroSeguir = confirm ('queres seguir comprando?');
+if (quieroSeguir) {
+    const nuevaCantidad =  Number(prompt('buenisimo!!! cuantos produtos mas deseas? '));
+    const nuevoPrecio = Number (prompt('ingrese el precio:'));
+    const precioFinal2 = (nuevaCantidad * nuevoPrecio);
+    alert ('el precio total de su compra es =' + ( precioFinal + precioFinal2) );
+    alert ('gracias por su compra, vuelva prontos...dijo Apu');
+     } else {
+    alert('gracias por su compra, vuelva prontos...dijo Apu');}
+};
+
